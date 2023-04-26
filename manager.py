67773bb09dfaa71d97a11bcd77bb0ee0be174fe0
MICHAŁ <mg814@wp.pl>
2023-04-26 15:10:26 +0200

    feat: manager

--- manager.py
+++ manager.py
@@ -24,9 +24,10 @@ class Manager:
         file_name = self.enter_file_name()
         self.file_handler.write_to_file(file_name, self.buffer)
 
-    def read(self):  # FIXME read_to_buffer.
+    def read_to_buffer(self) -> None:
         file_name = self.enter_file_name()
         my_list = self.file_handler.read_file(file_name)
+        self.buffer.clear_buffer()
         for item in my_list:
             self.buffer.add_data(item)
 
@@ -38,15 +39,24 @@ class Manager:
         return encryption_string
 
     def cesar_decrypted(self) -> list[dict]:
-        for idx, text in enumerate(self.buffer.data):
-            decryption_string = Rot.create_rot(text.get("rot_type")).rot_decryption(
-                text.get("text")
-            )
-            self.buffer.data[idx]["text"] = decryption_string
-            self.buffer.data[idx]["status"] = "decrypted"
-        print(self.buffer.data)
+        for text in self.buffer.data:
+            self.cesar_decrypted_buffer(text)
+        print(f"\n{self.buffer.data}")
         print(f"Odzszyfrowano dane.")
         return self.buffer.data
 
-    def add_to_buffer(self):
+    def cesar_decrypted_buffer(self, buffer_dict: dict) -> None:
+        if buffer_dict.get("rot_type") == "rot13":
+            decryption_string = Rot.create_rot(
+                buffer_dict.get("rot_type")
+            ).rot_decryption(buffer_dict.get("text"))
+        else:
+            decryption_string = Rot.create_rot(
+                buffer_dict.get("rot_type")
+            ).rot_encryption(buffer_dict.get("text"))
+
+        buffer_dict["text"] = decryption_string
+        buffer_dict["status"] = "decrypted"
+
+    def add_to_buffer(self) -> None:
         self.buffer.add_data(Text(self.string, "encrypted", self.rot_type).for_json())
--- menu.py
+++ menu.py
@@ -14,7 +14,6 @@ class Menu:
                 1: self.type_cipher_menu,
                 2: self.manager.write,
                 3: self.decryption_menu,
-                4: self.manager.buffer.data,
             },
             2: {1: "rot13", 2: "rot47"},
             3: {1: self.manager.cesar_decrypted, 2: self.manager.buffer.data},
@@ -28,18 +27,18 @@ class Menu:
 
         self.manager.cesar_encrypted(self.type_rot)
         self.manager.add_to_buffer()
+        print(self.manager.buffer.data)
 
     def main_menu(self) -> None:
         choice = int(
             input(
-                "\nMenu główne:\n1 - Zaszyfruj tekst\n2 - Zapis do pliku\n3 - Odczyt z pliku\n"
-                "4 - Pokaż dane gotowe do zapisu\n5 - Wyjdź\n"
+                "\nMenu główne:\n1 - Zaszyfruj tekst\n2 - Zapis do pliku\n3 - Odczyt z pliku\n4 - Wyjdź\n"
             )
         )
         self.execute(choice)
 
     def decryption_menu(self) -> None:
-        self.manager.read()
+        self.manager.read_to_buffer()
         choice = int(
             input("Czy chcesz odszyfrować zawartość pliku?\n1 - Tak\n2 - Nie\n")
         )
@@ -49,8 +48,6 @@ class Menu:
         if choice in range(1, 4, 1):
             self.options.get(1).get(choice)()
         elif choice == 4:
-            print(self.options.get(1).get(choice))
-        elif choice == 5:
             sys.exit(0)
 
     def type_menu_execute(self, choice: int) -> None:
