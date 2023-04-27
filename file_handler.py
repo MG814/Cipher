65b864ed4774dc148b5785e898533395c21134eb
MICHAŁ <mg814@wp.pl>
2023-04-27 10:15:29 +0200

    refactor: file_handler

--- file_handler.py
+++ file_handler.py
@@ -8,14 +8,11 @@ class FileHandler:
         with open(f"{file_name}{extension}", "w", encoding="utf-8") as file:
             json.dump(buffer.data, file)
 
-        print(f"Zapisano do pliku '{file_name}'")
-
     @staticmethod
     def read_file(file_name: str, extension: str = ".json") -> list[dict]:
         try:
             with open(f"{file_name}{extension}", "r", encoding="utf-8") as file:
                 data_list = json.load(file)
-                print(f"Odczytano dane z pliku '{file_name}'")
             return data_list
         except FileNotFoundError:
             print("Plik o podanej nazwie nie istnieje.")
